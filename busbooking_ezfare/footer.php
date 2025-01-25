c3b155b626081bbe37c2854e379bcd07ae384f5e
sajibnub929 <asrafuzzamanshojib2002@gmail.com>
2025-01-25 21:26:29 +0600

    Add files via upload

---
+++ busbooking_ezfare/footer.php
@@ -0,0 +1,61 @@
+<footer>
+        <div class="footerInfo">
+            <div class="footerContainer">
+                <div class="footerItem">
+
+                    <div class="socialInfo">
+                        <div class="mainTitle">
+                            <h4> Follow Us <span></span></h4>
+                        </div>
+                        <div class="socialItem">
+                            <div class="socialLink facebook">
+                                <i class="fab fa-facebook-f"></i>
+                            </div>
+                            <div class="socialLink instagram">
+                                <i class="fab fa-instagram"></i>
+                            </div>
+                            <div class="socialLink twitter">
+                                <i class="fab fa-twitter"></i>
+                            </div>
+                        </div>
+                    </div>
+<!--  -->
+                    <div class="socialInfo">
+                        <div class="mainTitle">
+                            <h4> Service <span></span></h4>
+                        </div>
+                        <div class="socialItem">
+                            <a href="#"> Online Ticket Booking </a>
+                            <a href="#"> Booking Modification</a>
+                            <a href="#"> Ticket cancellation </a>
+                            <a href="#"> Rating and Reviews </a>
+                        </div>
+                    </div>
+<!--  -->
+                    <div class="socialInfo">
+                        <div class="mainTitle">
+                            <h4> Company Info <span></span></h4>
+                        </div>
+                        <div class="socialItem">
+                            <a href="#"> Terms and Conditions </a>
+                            <a href="#"> FAQ</a>
+                            <a href="#"> Privacy Notice </a>
+                        </div>
+                    </div>
+<!--  -->
+                    <div class="socialInfo">
+                        <div class="mainTitle">
+                            <h4> Address <span></span></h4>
+                        </div>
+                        <h5 class="addressTitle">Dhaka, Bangladesh</h5>
+                        <div class="callInfo">
+                            <h6>Call</h6> <i class="fas fa-arrow-right"></i>
+                            <span>+8801783967440</span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </footer>
+
